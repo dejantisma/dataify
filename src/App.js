f26795c0794d5e300c1c40688adb71cdd984d0a3
Dejan Tisma <dejantisma@gmail.com>
2020-11-10 21:09:33 -0500

    cleared app

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> a save to reload.
-        </p>
-        <Button variant="primary" className="btn-primary">Primary</Button>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
   );
 }
 
