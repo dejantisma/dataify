b467d5fccfe32a72fe0ac3865f818e171113efe2
Dejan Tisma <dejantisma@gmail.com>
2020-12-01 18:10:13 -0500

    finished access token and structured pages for API calls to spotify to fetch data. added carousels as place holders for favorite artists (one of many more to come).

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Nav className="ml-auto">
             <Nav.Link href="https://github.com/dejantisma">GitHub</Nav.Link>
           </Nav>
-        </Navbar>  
+        </Navbar>
 
         <Switch>
           <Route path="/" exact component={Home}></Route>
@@ -40,42 +40,32 @@ function App() {
 }
 
 
-const Home = () => 
- 
-(
-  <div>
-    <br></br>
-    <br></br>
-    <h1>Welcome!</h1>
-    <h2>Let's see your Spotify data.</h2>
-    <br></br>
-    <Button onClick={openLogin}>
-  Login With Spotify
+const Home = () =>
+  (
+    <div>
+      <br></br>
+      <br></br>
+      <h1>Welcome!</h1>
+      <h2>Let's see your Spotify data.</h2>
+      <br></br>
+      <Button onClick={openLogin}>
+        Login With Spotify
     </Button>
-   {/* <Nav.Link href={window.open`https://accounts.spotify.com/authorize?scope=user-read-private%20user-read-email&client_id=${encodeURIComponent(client_id)}&response_type=token&redirect_uri=${encodeURI(redirect_uri)}`}>Yooo</Nav.Link> */}
+    
+    </div>
+  );
 
 
-  </div>
-);
-
-async function getAccessToken(e){
-
-
-  await openLogin();
-  console.log('finishing getAccessToken');
-
-  
-}
-async function openLogin(e){
-  var URL = `https://accounts.spotify.com/authorize?client_id=${encodeURIComponent(client_id)}&response_type=token&redirect_uri=${encodeURI(redirect_uri)}`;
-  window.location = URL; 
-  console.log("finishing openLogin");
+async function openLogin(e) {
+  var URL = `https://accounts.spotify.com/authorize?client_id=${encodeURIComponent(client_id)}&response_type=token&redirect_uri=${encodeURI(redirect_uri)}&scope=${scopes}`;
+  window.location = URL;
 }
 
 var client_id = '3f5e2b54cd754feaac87cc017616ba7e'; // Your client id
 var client_secret = 'CLIENT_SECRET'; // Your secret
 var redirect_uri = 'https://dataify.herokuapp.com/callback/'; // Your redirect uri
-var scopes = 'user-read-private user-read-email';
+//var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var scopes = 'user-read-private user-read-email user-read-recently-played user-top-read user-library-read user-follow-read user-read-currently-playing user-read-playback-state';
 
 
 
--- src/Callback.js
+++ src/Callback.js
@@ -1,40 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from 'react';
+import { Jumbotron, Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar } from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function Callback() {
     if (window.location.hash) {
         var access_token = new URLSearchParams(window.location.hash.substring(1)).get("access_token"); //Puts hash in variable, and removes the # character
         var token_type = new URLSearchParams(window.location.hash.substring(1)).get("token_type");
-       //console.log(new URLSearchParams(hash).get("access_token"));
-      //alert (hash);
-
-      if(access_token == null){
-          console.log(window.location.protocol);
-          console.log(window.location.host);
-          window.location = window.location.protocol+"//"+window.location.host;
-      }
-      console.log(access_token);
-      console.log(token_type);
-    } else {
+        if (access_token == null) { //hash but no token
+            console.log(window.location.protocol);
+            console.log(window.location.host);
+            window.location = window.location.protocol + "//" + window.location.host;
+        } else { //hash and token
+            console.log(access_token);
+            console.log(token_type);
+        }
+    } else { //no hash
+        console.log(window.location.protocol);
+        console.log(window.location.host);
+        window.location = window.location.protocol + "//" + window.location.host;
         console.log('no hash');
     }
 
 
+    const [email, setEmail] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const [topArtists, setArtists] = useState([]);
+    const [topArtistsAllTime, setArtistsAllTime] = useState([]);
+
+    // console.log("ACCESS TOKEN: " + access_token);
+
+    useEffect(() => {
+        try {
+            fetch('https://api.spotify.com/v1/me', {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    setEmail(data.email);
+                    setDisplayName(data.display_name);
+
+                })
+
+
+            fetch('https://api.spotify.com/v1/me/top/artists?time_range=short_term', {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data.items);
+                    setArtists(data.items);
+                    console.log(topArtists[0].images[0].url);
+                }).catch(error => { console.error('Error:' + error) })
+
+
+            fetch('https://api.spotify.com/v1/me/top/artists?time_range=long_term', {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data.items);
+                    setArtistsAllTime(data.items);
+                    console.log(topArtistsAllTime[0].images[0].url);
+
+                }).catch(error => { console.error('Error:' + error) })
+
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }, []);
+
 
     return (
-        <div className="App">
+        <div class="mx-auto">
+            <Jumbotron>
+                <h1>Hello, {displayName}!</h1>
+                <p>
+                    You logged in successfully. Below is a tour of <em>some</em> of your Spotify data, enjoy!
+                </p>
+                <p>
+                </p>
+            </Jumbotron>
 
-            <h1>
-                CALLBACK!!!
-      </h1>
+            <h3>Over the past month you've really liked these 5 artists..</h3>
+            <br></br>
+            <Carousel>
+                {topArtists.slice(0, 5).map(a =>
+                    <Carousel.Item key={a.name}>
+                        <img
+                            src={a.images[0].url}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{a.name}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                )}
+            </Carousel>
+
+            <br></br>
+            <h3>But all time, this is your top 5</h3>
+            <br></br>
+
+            <Carousel>
+                {topArtistsAllTime.slice(0, 5).map(a =>
+                    <Carousel.Item key={a.name}>
+                        <img
+                            src={a.images[0].url}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{a.name}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                )}
+            </Carousel>
 
         </div>
+
     );
+
 }
+
 
 
 export default Callback;
