850e1f0b15667747777d7600237b4acebb7fb833
Dejan Tisma <dejantisma@gmail.com>
2020-12-10 14:32:59 -0500

    modal to  search? and added artists API call and favicon/title

--- src/About.js
+++ src/About.js
@@ -1,18 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar } from 'react-bootstrap';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+
 
 function About() {
+
+ 
+
     return (
       <div className="App">
        
-      
-      <h1>
-          About us!!!
-      </h1>
+       <h1 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 200 }}>About</h1>
+
+       <div className="container">
+<p>
+
+If you're reading this, hopefully you got to play with your Spotify data a little! The purpose of this web-app was to not only show you what cool things could be built using your streaming data, but bring attention to how powerful and important big data is. I'll ana
+
+</p>
+<p>
+2) 8.17
+Cues that were missed or ignored in the Therac-25 case by the manufacturer were having error messages
+not really mean anything on the console. Obviously, there were limitations due to the hardware (not much
+memory back then) but at the very least there should be a manual that describes error codes that aren’t
+helpful. Cues that were missed from the users was failure to stop the operator due to an error. For
+example, some errors could happen and the machine would pause, but the operator could continue to turn
+on the electron beam by pressing a single button. Obviously internal code issues were the problem here,
+but allowing the operator to be lazy and bypass serious errors are a major concern.
+</p>
+
+<p>
+3) 8.29
+Risk management “includes being alert to cues that might indicate an error or problem,” this was a huge
+issue in the Therac-25 case as described in 8.17. Both the Therac-25 and Challenger cases had a lot of
+complacency by the operators. They couldn’t prove that the system was safe or not. In the Therac-25 case
+a worker went to an office party during the appointment and overdid the treatment, and in the Challenger
+case the cold weather was an issue so there’s a lot of real-world things that happen that aren’t accounted
+for. 
+</p>
+</div>
     
   
   
--- src/Callback.js
+++ src/Callback.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Link, useEffect, useState } from 'react';
-import { Jumbotron, Carousel, FormGroup, Alert, Form, Button, InputGroup, Dropdown, DropdownButton, FormControl } from 'react-bootstrap';
+import { CardGroup,Card, Modal, Carousel, Badge, Alert, Form, Button, InputGroup, Dropdown, DropdownButton, FormControl, CardDeck } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -23,6 +23,58 @@ function Callback() {
         //    console.log('no hash');
     }
 
+    const [email, setEmail] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const [topArtists, setArtists] = useState([]);
+    const [topArtistsAllTime, setArtistsAllTime] = useState([]);
+    const [buttonSearchTitle, setButtonSearchTitle] = useState('Artist');
+    const [searchInput, setSearchInput] = useState('');
+    const [searchReturn, setSearchReturn] = useState({});
+    const [uploadFile, setUploadFile] = useState();
+    const [streamingDataArr, setStreamingDataArr] = useState([]);
+    const [modalTitle, setModalTitle] = useState('');
+    const [timeListened,setTimeListened] = useState('');
+    const [totalStreams, setTotalStreams]=useState('');
+    const [firstDate,setFirstDate]=useState('');
+    const [lastDate,setLastDate]=useState('');
+    const [lastSong,setLastSong]=useState('');
+    const [firstSong,setFirstSong]=useState('');
+    const [imageURLModal,setImageURLModal]=useState('');
+    const [artist, setArtist] = useState('');
+    const [show, setShow] = useState(false);
+    const[song1IMG,setSong1IMG] = useState('');
+    const[song2IMG,setSong2IMG] = useState('');
+
+    const[song3IMG,setSong3IMG] = useState('');
+    const[song4IMG,setSong4IMG] = useState('');
+    const[topTracksAllTime,setTracksAllTime]=useState([])
+    const[topTracks,setTracks]=useState([])
+    const[topTracks6MO,setTracks6Mo]=useState([])
+
+
+
+    function timeConversion(millisec) {
+
+        var seconds = (millisec / 1000).toFixed(1);
+
+        var minutes = (millisec / (1000 * 60)).toFixed(1);
+
+        var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+
+        var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+        if (seconds < 60) {
+            return seconds + " seconds!";
+        } else if (minutes < 60) {
+            return minutes + " minutes!";
+        } else if (hours < 24) {
+            return hours + " hours!";
+        } else {
+            return days + " days.. or "+24*days+" hours!";
+        }
+    }
+    
+
 
     const handleSelect = (e) => {
         console.log(e);
@@ -41,58 +93,91 @@ function Callback() {
 
             )
 
-           // console.log(result);
-        }else{
+            // console.log(result);
+        } else {
             console.log('ADDING UP ALL THE TIME IN STREAMINGDATAARR FOR ' + name);
             console.log(streamingDataArr);
             var result = streamingDataArr.filter(obj => {
-              //  console.log(obj.artistName);
+                //  console.log(obj.artistName);
                 return obj.artistName === name;
             })
 
-          //  console.log(result);
+            //  console.log(result);
 
         }
 
-
         console.log(result);
-        console.log('TOTAL='+result.reduce((a,b) => +a + +b.msPlayed,0));
+
+        console.log('TOTAL=' + result.reduce((a, b) => +a + +b.msPlayed, 0));
+        setTimeListened(result.reduce((a, b) => +a + +b.msPlayed, 0));
+        setTotalStreams(result.length);
+
+        console.log('sorted by date');
+        result.sort(function(a,b){
+            return new Date(b.endTime)- new Date(a.endTime);
+        });
+
+        setLastDate(result[0].endTime);
+        setFirstDate(result[result.length-1].endTime);
+        if(type === 'artist'){
+            setLastSong(' ('+result[0].trackName+')');
+            setFirstSong(' ('+result[result.length-1].trackName+')');
+            console.log('last = '+lastSong);
+        }else{
+            setArtist(' by '+result[0].artistName);
+            setLastSong('');
+            setFirstSong('');
+        }
+        console.log(result);
+
     }
 
     const search = (e) => {
-        try {
-            fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchInput)}&type=${buttonSearchTitle.toLowerCase() === 'song' ? 'track' : buttonSearchTitle.toLowerCase()}`, {
-                headers: {
-                    Accept: "application/json",
-                    Authorization: `Bearer ${access_token}`,
-                    "Content-Type": "application/json",
 
+        fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchInput)}&type=${buttonSearchTitle.toLowerCase() === 'song' ? 'track' : buttonSearchTitle.toLowerCase()}`, {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${access_token}`,
+                "Content-Type": "application/json",
+
+            }
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong');
+            }
+
+        }).then(data => {
+                if (buttonSearchTitle.toLowerCase() === 'song') {
+                    console.log(data.tracks?.items[0]);
+                    setSearchReturn(data.tracks?.items[0])
+                    console.log('Adding up all ' + data.tracks?.items[0].name + ' streams');
+                    console.log('arr in search');
+                    console.log(typeof streamingDataArr);
+                    addTime(data.tracks?.items[0].name, 'track');
+                    setModalTitle(data.tracks?.items[0].name);
+                   setImageURLModal('');
+
+
+                    // } else if (buttonSearchTitle.toLowerCase() === 'artist') {
+                    //     console.log(data.artists?.items[0]);
+                    //     setSearchReturn(data.artists?.items[0])
+                } else {
+                    console.log(data);
+                    
+                    setSearchReturn(data.artists?.items[0])
+                    //console.log('Adding up all ' + data.artists?.items[0].name + ' streams');
+                    addTime(data.artists?.items[0].name, 'artist');
+                   setImageURLModal(data.artists?.items[0]?.images[0]?.url);
+                    setModalTitle(data.artists?.items[0].name);
                 }
-            }).then(response => response.json())
-                .then(data => {
-                    if (buttonSearchTitle.toLowerCase() === 'song') {
-                        console.log(data.tracks?.items[0]);
-                        setSearchReturn(data.tracks?.items[0])
-                        console.log('Adding up all ' + data.tracks?.items[0].name + ' streams');
-                        console.log('arr in search');
-                        console.log(typeof streamingDataArr);
-                        addTime(data.tracks?.items[0].name, 'track');
+                handleShow();
+            }).catch((error) => {
+                console.log('middle catch');
+                console.log(error);
+            })
 
-
-                        // } else if (buttonSearchTitle.toLowerCase() === 'artist') {
-                        //     console.log(data.artists?.items[0]);
-                        //     setSearchReturn(data.artists?.items[0])
-                    } else {
-                        console.log(data.artists?.items[0]);
-                        setSearchReturn(data.artists?.items[0])
-                        console.log('Adding up all ' + data.artists?.items[0].name + ' streams');
-                        addTime(data.artists?.items[0].name, 'artist');
-                    }
-                })
-        }
-        catch (error) {
-            console.log(error);
-        }
     }
 
 
@@ -118,15 +203,11 @@ function Callback() {
 
     }
 
-    const [email, setEmail] = useState('');
-    const [displayName, setDisplayName] = useState('');
-    const [topArtists, setArtists] = useState([]);
-    const [topArtistsAllTime, setArtistsAllTime] = useState([]);
-    const [buttonSearchTitle, setButtonSearchTitle] = useState('Artist');
-    const [searchInput, setSearchInput] = useState('');
-    const [searchReturn, setSearchReturn] = useState({});
-    const [uploadFile, setUploadFile] = useState();
-    const [streamingDataArr, setStreamingDataArr] = useState([]);
+    
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
 
     // console.log("ACCESS TOKEN: " + access_token);
 
@@ -176,6 +257,55 @@ function Callback() {
                 }).catch(error => { console.error('Error:' + error) })
 
 
+
+                fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term', {
+                    headers: {
+                        Accept: "application/json",
+                        Authorization: `Bearer ${access_token}`,
+                        "Content-Type": "application/json"
+                    }
+                }).then(response => response.json())
+                    .then(data => {
+                        console.log("TRACKS RECENT");
+                          console.log(data.items);
+                        setTracks(data.items);
+                        //   console.log(topArtistsAllTime[0].images[0].url);
+    
+                    }).catch(error => { console.error('Error:' + error) })
+
+
+                fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term', {
+                    headers: {
+                        Accept: "application/json",
+                        Authorization: `Bearer ${access_token}`,
+                        "Content-Type": "application/json"
+                    }
+                }).then(response => response.json())
+                    .then(data => {
+                      //  console.log("TRACKS ALL TIME");
+                          console.log(data.items);
+                        setTracksAllTime(data.items);
+                        //   console.log(topArtistsAllTime[0].images[0].url);
+    
+                    }).catch(error => { console.error('Error:' + error) })
+
+
+                    fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term', {
+                        headers: {
+                            Accept: "application/json",
+                            Authorization: `Bearer ${access_token}`,
+                            "Content-Type": "application/json"
+                        }
+                    }).then(response => response.json())
+                        .then(data => {
+                          //  console.log("TRACKS ALL TIME");
+                              console.log(data.items);
+                            setTracks6Mo(data.items);
+                            //   console.log(topArtistsAllTime[0].images[0].url);
+        
+                        }).catch(error => { console.error('Error:' + error) })
+
+
         }
         catch (error) {
             console.log(error);
@@ -196,15 +326,6 @@ function Callback() {
                         You logged in successfully. Below is a tour of <em>some</em> of your Spotify data, enjoy!</p>
                 </Alert>
             </div>
-            {/* 
-            <Jumbotron>
-                <h1>Hello, {displayName}!</h1>
-                <p>
-                    You logged in successfully. Below is a tour of <em>some</em> of your Spotify data, enjoy!
-                </p>
-                <p>
-                </p>
-            </Jumbotron> */}
 
             <h1 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 200 }}>Artists</h1>
             <h3 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 40 }}>Recently..</h3>
@@ -244,11 +365,101 @@ function Callback() {
             </Carousel>
 
             <hr></hr>
+            <h1 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 200 }}>Songs</h1>
+
+<div className="container">
+
+
+<h3 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 40 }}>Recently (1 mo.)</h3>
+
+<CardDeck>
+{topTracks?.slice(0,5).map(a=>
+    <Card>
+        <a href={a.external_urls.spotify}><Card.Img variant="top" src={a.album.images[0].url}/></a>
+<Card.Body>
+    <Card.Title>{a.name}</Card.Title>
+    <Card.Text>{a.artists[0].name}</Card.Text>
+</Card.Body>
+    </Card>
+    
+    )}
+</CardDeck>
+
+<br></br>
+
+<h3 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 40 }}>Recently (6 mo.)</h3>
+
+<CardDeck>
+{topTracks6MO?.slice(0,5).map(a=>
+    <Card>
+        <a href={a.external_urls.spotify}><Card.Img variant="top" src={a.album.images[0].url}/></a>
+<Card.Body>
+    <Card.Title>{a.name}</Card.Title>
+    <Card.Text>{a.artists[0].name}</Card.Text>
+</Card.Body>
+    </Card>
+    
+    )}
+</CardDeck>
+
+<br></br>
+
+<h3 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 40 }}>All time</h3>
+<CardDeck>
+{topTracksAllTime?.slice(0,5).map(a=>
+    <Card>
+        <a href={a.external_urls.spotify}><Card.Img variant="top" src={a.album.images[0].url}/></a>
+<Card.Body>
+    <Card.Title>{a.name}</Card.Title>
+    <Card.Text>{a.artists[0].name}</Card.Text>
+</Card.Body>
+    </Card>
+    
+    )}
+</CardDeck>
+
+                {/* <CardDeck>
+                    <Card border="primary" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={song1IMG} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card border="success" style={{ width: '18rem' }}>
+                        <Card.Img variant="bottom" src={song2IMG} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card border="danger" style={{ width: '18rem' }} >
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardDeck> */}
+
+            </div>
+
+
+            <hr></hr>
             <h1 style={{ fontFamily: 'Nerko One', textAlign: "center", fontSize: 200 }}>Upload</h1>
 
             <div class="container">
                 <Alert variant="success">
-                    <Alert.Heading>Upload your Spotify data</Alert.Heading>
+                    <Alert.Heading>Upload your Spotify data <Badge variant="success" pill>New</Badge> </Alert.Heading>
                     <p>
                         Unfortunately, the Spotify Web API is limited in the data that it provides. You saw how we could use it above, but unfortunately the only sort of history or playback accessible via the API is the last 50 tracks. You can access all time personalization like we did above, but there's no detail in terms of how many tracks streamed or hours or what tracks exactly. To view more data you have to request it from Spotify via https://www.spotify.com/us/account/privacy/,
                         you're able to download your Spotify data over the last year including streaming history. Keep in mind once you request your data it may take a few days to process before you can download it.</p>
@@ -282,8 +493,6 @@ function Callback() {
                         size="lg"
                     >
                         <Dropdown.Item eventKey="1" >Artist</Dropdown.Item>
-                        {//  <Dropdown.Item eventKey="2">Album</Dropdown.Item> 
-                        }
                         <Dropdown.Item eventKey="2">Song</Dropdown.Item>
                     </DropdownButton>
 
@@ -295,7 +504,43 @@ function Callback() {
 
             </div>
 
+
+
+
+
+            <Modal  aria-labelledby="contained-modal-title-vcenter"
+      centered show={show} onHide={handleClose}
+      >
+                <Modal.Header closeButton>
+                    <Modal.Title>{modalTitle}{artist}</Modal.Title>
+                </Modal.Header>
+                    <Modal.Body>
+                        <img src={imageURLModal} className="img-fluid">
+                        </img>
+                   
+                   {imageURLModal !=''? ( //spocing if image or not (have an extra line break)
+                       <div>
+                           <br></br>
+                       </div>
+                   ):(  
+                    null
+                   )}                       
+                        You've listened <b>{totalStreams}</b> times for a total of {timeListened}ms. That's {timeConversion(timeListened)}
+                    <hr></hr>
+                    You first listened to {modalTitle} on {firstDate}{firstSong} and most recently on {lastDate}{lastSong}!
+                    </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>
+                        Close
+              </Button>
+                </Modal.Footer>
+            </Modal>
+
+
         </div>
+
+
 
     );
 
